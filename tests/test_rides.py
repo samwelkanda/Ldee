615e28404d248bb635062e1cb0b260e83dce3351
Samwel Kanda <samwelkanda@gmail.com>
2018-06-26 13:37:52 +0300

    [Feature #158624375] Test that request to get all rides works as expected

--- tests/test_rides.py
+++ tests/test_rides.py
@@ -1,1 +1,11 @@
 # Tests for rides endpoints
+
+import json
+
+
+def test_get_rides(test_client):
+    """
+    Test that get request works correctly
+    """
+    response = test_client.get('/api/v1/rides')
+    assert response.status_code == 200
