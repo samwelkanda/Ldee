321b63d5ec9c2f4cd16def51bd4b8a1b25c89b1a
Samwel Kanda <samwelkanda@gmail.com>
2018-06-26 14:04:28 +0300

    [Feature #158624385] Test request returns the specific ride

--- tests/test_rides.py
+++ tests/test_rides.py
@@ -9,3 +9,12 @@ def test_get_rides(test_client):
     """
     response = test_client.get('/api/v1/rides')
     assert response.status_code == 200
+
+def test_get_single_ride(test_client):
+    """
+    Test request returns correct ride with specified ID
+    """
+
+    response = test_client.get('/api/v1/rides/1')
+    assert response.status_code == 200
+    
